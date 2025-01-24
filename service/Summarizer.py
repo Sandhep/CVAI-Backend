60ecaea6fd773cc33d136ecac33c2ffafc2dff33
Sandhep <b.sandhep111@gmail.com>
2025-01-24 10:36:16 +0530

    Text summarizer

---
+++ service/Summarizer.py
@@ -0,0 +1,16 @@
+from transformers import AutoTokenizer, AutoModelWithLMHead
+
+from transformers import pipeline
+
+summarizer = pipeline("summarization", model="Falconsai/text_summarization")     
+
+class Summarizer:
+
+    def summarize(text):
+        l = len(text)
+        if l<1000:
+            maxlength = l/1.5
+        else:
+            maxlength = 1000
+        summary = summarizer(text, max_length=maxlength, min_length=30, do_sample=False)                
+        return summary[0]['summary_text']
